d65b637be4386feff70695c1ab2c0f1aeb4bf351
DarkDot <darkestdot@gmail.com>
2014-11-17 14:40:02 -0600

    part 1

---
+++ as7/sorts.h
@@ -0,0 +1,28 @@
+template <class T>
+void buildList(vector<T>& set, const char* fileName)
+{
+//This function should read items from an input file and put them into a vector. The first argument to this function is a reference to a vector object that will be used to store the items. The second argument is a C-style string containing the full pathname of the input file.
+
+//The function should first open the file for input, then read items from the file using the >> operator one at a time until end of file, inserting them into the vector. Finally, it should close the input file. Here's some pseudocode for the logic:
+
+T item;
+ifstream inFile;
+
+inFile.open(fileName);
+if (!inFile){
+cout << endl << "File "<<inFile<<" not found"<<endl;
+exit(1);                //running away now!
+}
+inFile>>item;
+
+while (inFile)
+{
+set.push_back(item);
+inFile>>item;
+}
+inFile.close();
+}
+template <class T>
+void printList(const vector<T>& set, int itemWidth, int numPerLine){
+
+}
